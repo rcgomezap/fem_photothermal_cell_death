7f64a2e28bfe8130b8ce71219c2eb91b3e826afc
rcgomezap <rcgomezap@gmail.com>
2024-05-10 08:41:39 -0500

    Organized project structure

--- FEM/bridge/run_fenicsx.py
+++ FEM/bridge/run_fenicsx.py
@@ -1,19 +1,16 @@
 import subprocess as su
 import os
-dir1 = os.path.dirname(os.path.realpath(__file__))[:-11]
-
+root = os.path.dirname(os.path.realpath(__file__))[:-11]
+dir = os.getcwd()
+dir = os.path.relpath(dir, root)
 
 def clean_results(dir): # Limpia los resultados
     su.run("rm -rf FEM/fenicsx/results/*", shell=True)
 
-def run_simultation(dir): # Genera la malla y corre la simulación
-    dir = os.path.relpath(dir, dir1)
-    # print(dir)
-    # clean_results()
+def run_simultation(): # Genera la malla y corre la simulación
     # su.run(f"docker run --rm -i -v $(pwd):/root dolfinx_v0.6.0:rc bash -c 'gmsh mesh/msh_lopes.geo -3 -nt 12 -cpu -o mesh/msh/tumor_geometry.msh' | tee log_gmsh.txt", shell=True)
-    su.run(f"docker run --rm -i -v {dir1}:/root dolfinx_v0.6.0:rc bash -c 'python3 FEM/fenicsx/mesh/mesh_convert.py'", shell=True)
-    su.run(f"docker run --rm -it -v {dir1}:/root dolfinx_v0.6.0:rc bash -c 'python3 {dir}/fenicsx/fenicsx_main.py {dir}' | tee log_fenicsx.txt", shell=True)
-# save_results(f"factor_{list_factor[i]}")
+    su.run(f"docker run --rm -i -v {root}:/root dolfinx_v0.6.0:rc bash -c 'python3 FEM/fenicsx/mesh/mesh_convert.py'", shell=True)
+    su.run(f"docker run --rm -it -v {root}:/root dolfinx_v0.6.0:rc bash -c 'python3 {dir}/fenicsx/fenicsx_main.py {dir}' | tee log_fenicsx.txt", shell=True)
 
 # run_simultation("runs/validacion_soni")
 # print("Hello World")
--- runs/validacion_soni/main.py
+++ runs/validacion_soni/main.py
@@ -4,15 +4,12 @@ Created on Sat Feb 10 09:56:34 2024
 
 @author: Melany Martinez
 """
-import sys
-import os
-dir = os.path.dirname(os.path.realpath(__file__))
-dir1 = os.path.abspath('../..')
-sys.path.append(dir1)
-
 import numpy as np
+
+import sys
+sys.path.append('../../')
 from FEM.bridge.run_fenicsx import run_simultation
 
-# run_simultation(dir)
+run_simultation()
 T = np.load("T.npy")
 print(T.max())
